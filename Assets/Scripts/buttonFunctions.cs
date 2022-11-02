5a32eeecc9ed1f3b09a10ab2fc15910a052bddb1
UnmadeThoughts <KGNSL@outlook.com>
2022-11-02 11:25:18 -0400

    Menu System & Game Manager Added

---
+++ Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonFunctions : MonoBehaviour
+{
+    public void resume()
+    {
+        gameManager.instance.unpause();
+        gameManager.instance.pauseMenu.SetActive(false);
+        gameManager.instance.isPaused = false;
+    }
+
+    public void restart()
+    {
+        gameManager.instance.unpause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance; // Singleton for reference in other methods
+
+    [Header("----- Player Information -----")]
+    public GameObject player; // Player entity tracker
+    public playerController playerScript; // Player script tracker
+
+    [Header("----- UI -----")]
+    public GameObject pauseMenu; //UI element for the pause screen
+
+    public bool isPaused;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this; // Binds the singleton on startup
+        player = GameObject.FindGameObjectWithTag("Player"); // Find the Player & bind
+        playerScript = player.GetComponent<playerController>(); // Find the playerController & bind
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel"))
+        {
+            isPaused = !isPaused;
+            pauseMenu.SetActive(isPaused);
+            if (isPaused)
+                pause();
+            else
+                unpause();
+        }
+    }
+
+    public void pause()
+    {
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void unpause()
+    {
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
