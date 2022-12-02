7d20b62f101141f08192d92c81df410ad33e51be
Hunter Ruppe <HRRuppe@student.fullsail.edu>
2022-12-02 01:26:37 -0500

    - added settings menu into main menu - added an audio mixer with sliders

---
+++ Assets/menuController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class menuController : MonoBehaviour
+{
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject settingsMenu;
+
+    [SerializeField] AudioMixer mixer; 
+    
+    [SerializeField] Slider masterSlider;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider sfxSlider;
+
+    public void openSettings()
+    {
+        settingsMenu.SetActive(true);
+        mainMenu.SetActive(false);  
+    }
+
+    public void closeSettings()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);  
+    }
+
+    public void MasterSliderChanged()
+    {
+        if (masterSlider.value <= 0)
+            mixer.SetFloat("Master Volume", -80.0f);
+        else 
+            mixer.SetFloat("Master Volume", Mathf.Log10(masterSlider.value) * 20); 
+    }
+
+    public void MusicSliderChanged()
+    {
+        if (musicSlider.value <= 0)
+            mixer.SetFloat("Music Volume", -80.0f);
+        else
+            mixer.SetFloat("Music Volume", Mathf.Log10(musicSlider.value) * 20);
+    }
+
+    public void SFXSliderChanged()
+    {
+        if (sfxSlider.value <= 0)
+            mixer.SetFloat("SFX Volume", -80.0f);
+        else
+            mixer.SetFloat("SFX Volume", Mathf.Log10(sfxSlider.value) * 20);
+    }
+}
